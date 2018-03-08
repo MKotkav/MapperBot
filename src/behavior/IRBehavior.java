d3bcc1094f4edebe6a1ac37087ab014bd69fd829
Mikael Kotkavuori <mikael.kotkavuori@gmail.com>
2018-03-08 02:45:19 +0200

    Initial commit

---
+++ src/behavior/IRBehavior.java
@@ -0,0 +1,64 @@
+package behavior;
+
+import lejos.hardware.Sound;
+import lejos.robotics.subsumption.Behavior;
+import server.ServerController;
+import server.utility.Data;
+
+public class IRBehavior implements Behavior {
+
+	private volatile boolean suppressed = false;
+	ServerController control;
+
+	public IRBehavior(ServerController control)
+	{
+		this.control = control;
+	}
+
+	/**
+	 * Takes control if the infrared sensor detects distances under 15
+	 */
+	public boolean takeControl() {
+		if(control.getIRDirection() > -20 && control.getIRDirection() < 20) {
+			return control.getDistance() < 20;
+		}
+		else return false;
+	}
+
+	/**
+	 * Writes the received distances and turns either left, right or 180 d
+	 * egrees depending on the side and front distances
+	 */
+	public void action() {
+		suppressed = false;
+		Sound.setVolume(20);
+		Sound.playNote(Sound.XYLOPHONE, 440, 50);
+		float[] array = control.getSidesDistance();
+		Data data = control.getLocation();
+		data.setDistanceForward(array[0]);
+		data.setDistanceRight(array[1]);
+		data.setDistanceLeft(array[2]);
+		control.writeData(data);
+
+		if (data.getDistanceLeft() > 30 && data.getDistanceRight() > 30) {
+			control.randTurn();
+			control.waitForStop();
+		}
+		else if (data.getDistanceLeft() > 25 ) {
+			control.turn("left");
+			control.waitForStop();
+		}
+		else if (data.getDistanceRight() > 25) {
+			control.turn("right");
+			control.waitForStop();
+		}
+		else {
+			control.turnHalf();
+			control.waitForStop();
+		}
+	}
+
+	public void suppress() {
+		suppressed = true;
+	}
+}
